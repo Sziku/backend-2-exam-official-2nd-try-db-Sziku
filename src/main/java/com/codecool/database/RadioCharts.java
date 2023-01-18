22cbfadad9f8ef2859e6a0c3af8e19bca3a182f1
Sziku <105989158+Sziku@users.noreply.github.com>
2023-01-18 14:28:19 +0100

    add methods two test failed

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -17,11 +17,60 @@ public class RadioCharts {
 
 
     public List<String> getArtists() {
+        final String SQL = "SELECT artist from music_broadcast group by artist order by artist ASC;";
+
+        try(Connection con = DriverManager.getConnection(dbUrl,dbUser,dbPassword)){
+            PreparedStatement st = con.prepareStatement(SQL);
+            ResultSet rs = st.executeQuery();
+
+            List<String> artist = new ArrayList<>();
+
+            while (rs.next()){
+                artist.add(rs.getString(1));
+            }
+            return artist;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public String getMostPlayedSong() {
+        final String SQL = "SELECT song, times_aired from music_broadcast group by song, times_aired order by times_aired DESC limit 1; ";
+
+        try(Connection con = DriverManager.getConnection(dbUrl,dbUser,dbPassword)){
+            PreparedStatement st = con.prepareStatement(SQL);
+            ResultSet rs = st.executeQuery();
+
+
+            while (rs.next()){
+                return rs.getString(1);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 
     public String getMostActiveArtist() {
+        final String SQL = "select artist, count(artist) as mostSong from music_broadcast group by artist order by count(artist) limit 1";
+
+        try(Connection con = DriverManager.getConnection(dbUrl,dbUser,dbPassword)){
+            PreparedStatement st = con.prepareStatement(SQL);
+            ResultSet rs = st.executeQuery();
+
+
+            while (rs.next()){
+                return rs.getString(1);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
